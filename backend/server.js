9664626232428499c7acb9ac93ab2ca0b647eabe
aditi38555 <aditiverma38555@gmail.com>
2026-05-05 17:10:49 +0530

    fixed db connection

--- backend/server.js
+++ backend/server.js
@@ -10,4 +10,12 @@ app.use(express.json());
 
 app.use("/api", invoiceRoutes);
 
-app.listen(5000, () => console.log("Server running 🚀"));
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
