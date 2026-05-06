e21843eb8a733ce830d0a7bfc669f795925b6f13
aditi38555 <aditiverma38555@gmail.com>
2026-05-06 16:18:32 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -30,4 +30,58 @@ db.connect((err) => {
   }
 });
 
+
+const mysql = require("mysql2");
+require("dotenv").config();
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+// const db = mysql.createConnection(process.env.DATABASE_URL);
+if (!process.env.DATABASE_URL) {
+  console.log("❌ DATABASE_URL missing");
+  process.exit(1);
+}
+
+const url = new URL(process.env.DATABASE_URL); 
+const db = mysql.createConnection({
+  host: url.hostname,
+  user: url.username,
+  password: url.password,
+  database: url.pathname.replace("/", ""),
+  port: url.port,
+});
+
+
+db.connect((err) => {
+  if (err) {
+    console.log("DB Error ❌", err);
+  } else {
+    console.log("MySQL Connected ✅");
+  }
+});
+
 module.exports = db;
+
+
+db.query(`
+  CREATE TABLE IF NOT EXISTS invoices (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(255),
+    amount DECIMAL(10,2),
+    date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+`, (err) => {
+  if (err) {
+    console.log("Table Error ❌", err);
+  } else {
+    console.log("Table ready ✅");
+  }
+});
+
+module.exports = db;
+
+
